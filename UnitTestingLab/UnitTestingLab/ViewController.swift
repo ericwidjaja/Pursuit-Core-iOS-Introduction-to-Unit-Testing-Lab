bd6560961c95b5797f76472b92aa0673db150b6e
Eric Widjaja <ericwidjaja@pursuit.org>
2019-08-29 17:17:13 -0400

    created json and swift model files for 'joke API'

---
+++ UnitTestingLab/UnitTestingLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnitTestingLab
+//
+//  Created by Eric Widjaja on 8/29/19.
+//  Copyright © 2019 Eric Widjaja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
